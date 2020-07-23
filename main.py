f404f0a5f9dd6ee415e441d9d47322f7f203ede5
Kyle Herstad <Herstky@gmail.com>
2020-07-23 11:08:24 -0700

    Implemented Joiner Module.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 
 from event import Event
-from module import Module, Source, Sink, Splitter
+from module import Module, Source, Sink, Splitter, Joiner
 from stream import Stream
 
 
@@ -15,10 +15,17 @@ if __name__ == '__main__':
     sink1 = Sink('Sink1')
     sink2 = Sink('Sink2')
     splitter1 = Splitter('Splitter1')
-    Stream(source.outlet_connections[0], splitter.inlet_connections[0])
-    Stream(splitter.outlet_connections[0], sink1.inlet_connections[0])
-    Stream(splitter.outlet_connections[1], sink2.inlet_connections[0])
-    modules = [source, splitter, sink1, sink2]
+    splitter2 = Splitter('Splitter2')
+    joiner1 = Joiner('Joiner1')
+
+    Stream(source1.outlet_connections[0], joiner1.inlet_connections[0])
+    Stream(splitter2.outlet_connections[1], joiner1.inlet_connections[1])
+    Stream(splitter1.outlet_connections[0], sink1.inlet_connections[0])
+    Stream(splitter1.outlet_connections[1], splitter2.inlet_connections[0])
+    Stream(splitter2.outlet_connections[0], sink2.inlet_connections[0])
+    Stream(joiner1.outlet_connections[0], splitter1.inlet_connections[0])
+
+    modules = [source1, splitter1, sink1, sink2, splitter2, joiner1]
     while True:
         loop(modules)
         input('Press enter to continue')
--- module.py
+++ module.py
@@ -60,10 +60,8 @@ class Splitter(Module):
         super().__init__(name)
         self.split_fraction = split_fraction
         self.add_inlet_connection(Connection(self, inlet_capacity, 'Inlet'))
-        self.add_outlet_connection(Connection(
-            self, inlet_capacity * split_fraction, 'Outlet1'))
-        self.add_outlet_connection(Connection(
-            self, inlet_capacity * (1 - split_fraction), 'Outlet2'))
+        self.add_outlet_connection(Connection(self, inlet_capacity * split_fraction, 'Outlet1'))
+        self.add_outlet_connection(Connection(self, inlet_capacity * (1 - split_fraction), 'Outlet2'))
 
     def process(self):
         inlet_connection, *_ = self.inlet_connections
@@ -71,14 +69,33 @@ class Splitter(Module):
 
         # Move events from inlet connection to the outlet connections
         inlet_amount = inlet_connection.transfer_to_module()
-        split_amount1 = outlet_connection1.transfer_from_module(
-            self.queue.amount_queued * self.split_fraction)
-        split_amount2 = outlet_connection2.transfer_from_module(
-            self.queue.amount_queued)
+        split_amount1 = outlet_connection1.transfer_from_module(self.queue.amount_queued * self.split_fraction)
+        split_amount2 = outlet_connection2.transfer_from_module(self.queue.amount_queued)
 
-        print(
-            f'{self.name} split {inlet_amount} into streams of {split_amount1} and {split_amount2}')
+        print(f'{self.name} split stream of {inlet_amount} into streams of {split_amount1} and {split_amount2}')
 
         # Push events from the outlet connections to inlet of module connected by stream
         outlet_connection1.push()
         outlet_connection2.push()
+
+
+class Joiner(Module):
+    def __init__(self, name='Joiner', inlet_capacity1=500, inlet_capacity2=500):
+        super().__init__(name)
+        self.add_inlet_connection(Connection(self, inlet_capacity1))
+        self.add_inlet_connection(Connection(self, inlet_capacity2))
+        self.add_outlet_connection(Connection(self, inlet_capacity1 + inlet_capacity2))
+
+    def process(self):
+        inlet_connection1, inlet_connection2, *_ = self.inlet_connections
+        outlet_connection1, *_ = self.outlet_connections
+
+        # Transfer events from the inlet connections to the outlet connection
+        inlet_amount1 = inlet_connection1.transfer_to_module()
+        inlet_amount2 = inlet_connection2.transfer_to_module()
+        joined_amount = outlet_connection1.transfer_from_module()
+
+        print(f'{self.name} joined streams of {inlet_amount1} and {inlet_amount2} into stream of {joined_amount}')
+
+        # Push events from the outlet connection to inlet of module connected by stream
+        outlet_connection1.push()
